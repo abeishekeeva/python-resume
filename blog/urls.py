63be1fee405134dd1aac3e3537498f60cb604b65
Aidai Beishekeeva <ABeyshekeeva@skymobile.local>
2019-02-06 17:59:16 +0600

    changing blog view

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('general', views.post_list_other, name='post_list_general'),
+    path('books', views.post_list_books, name='post_list_books'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,10 @@ def post_list_other(request):
     posts_other = Post.objects.filter(published_date__lte=timezone.now(), tag="general").order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts_other': posts_other})
 
+def post_list_books(request):
+    posts_books = Post.objects.filter(published_date__lte=timezone.now(), tag="books").order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts_other': posts_books})
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
