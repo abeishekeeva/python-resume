d5378077ac65323692bc29b6a413a1ee4ecc0592
Aidai Beishekeeva <ABeyshekeeva@skymobile.local>
2019-02-06 17:47:25 +0600

    blog view changed

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('general', views.post_list_other, name='post_list_general'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
